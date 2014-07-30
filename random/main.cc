e3694964b1fabf5d82b7416298fa35524baa5d37
Llisper Zhang <llisperzhang@gmail.com>
2014-07-31 00:45:08 +0800

    working on it

--- random/main.cc
+++ random/main.cc
@@ -4,7 +4,14 @@
 #include <algorithm>
 #include <stdint.h>
 #include <assert.h>
+#include <string.h>
+#include <math.h>
 #include <vector>
+#include <functional>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 
 struct Rand {
   Rand(uint32_t s) {
@@ -48,18 +55,21 @@ struct Player {
   std::vector<int> val_cnt;
 };
 
-bool can_play(const Player& p) { return p.num; }
-
 static int range;
 static int num;
 static int num_players;
 static int shuffle;
 static int global_dice;
 static double disturb;
+static int disturb_factor;
+static int split;
+
+// static void variance(std::ostream& os, const std::vector<int>& vals, int range);
+static double statistics(std::ostream& os, const std::vector<int>& cnt, int split);
 
 int main(int argc, char *argv[]) {
   if (argc < 6) {
-    printf("%s range num num_players shuffle global_dice disturb\n", argv[0]);
+    printf("%s range num num_players shuffle global_dice disturb disturb_factor split\n", argv[0]);
     return 0;
   }
 
@@ -69,23 +79,25 @@ int main(int argc, char *argv[]) {
   shuffle = atoi(argv[4]);
   global_dice = atoi(argv[5]);
   disturb = atof(argv[6]);
+  disturb_factor = atoi(argv[7]);
+  split = atoi(argv[8]);
 
   int total = num * num_players;
   int num_disturb = 0;
   Rand aux_dice((uint32_t)time(NULL));
   Rand g_dice((uint32_t)(time(NULL) + aux_dice()));
-  std::vector<Player> players;
+  std::vector<Player*> players;
 
   for (int i = 0; i < num_players; ++i) {
     Rand *dice = global_dice ? &g_dice : new Rand((uint32_t)(time(NULL) + aux_dice()));
-    players.push_back(Player(range, num, dice, !global_dice));
+    players.push_back(new Player(range, num, dice, !global_dice));
   }
 
   for (int i = 0; i < total; ++i) {
     std::vector<Player*> players_left;
     for (size_t k = 0; k < players.size(); ++k)
-      if (players[k].num)
-        players_left.push_back(&players[k]);
+      if (players[k]->num)
+        players_left.push_back(players[k]);
 
     Player *p = NULL;
     while (!p) {
@@ -97,25 +109,70 @@ int main(int argc, char *argv[]) {
     p->Random();
 
     if (global_dice && aux_dice.DRand() < disturb) {
-      ++num_disturb;
-      g_dice();
+      for (int i = 0; i < disturb_factor; ++i) {
+        ++num_disturb;
+        g_dice();
+      }
     }
   }
 
-  int *g_count = new int[range];
-  memset(g_count, 0, sizeof(int) * range);
+  std::vector<int> g_count(range, 0);
+  double avg_variance = 0.0;
   assert(disturb != 0 || num_disturb == 0);
   for (size_t i = 0; i < players.size(); ++i) {
-    assert(players[i].num == 0);
+    assert(players[i]->num == 0);
+    assert(num == std::accumulate(players[i]->val_cnt.begin(), players[i]->val_cnt.end(), 0));
+    avg_variance += statistics(std::cerr, players[i]->val_cnt, split);
+    std::transform(players[i]->val_cnt.begin(), players[i]->val_cnt.end(),
+                   g_count.begin(), g_count.begin(),
+                   std::plus<int>());
+  }
+  avg_variance /= players.size();
+  std::cout << '[' << avg_variance << "]\n";
 
+  assert(total == std::accumulate(g_count.begin(), g_count.end(), 0));
+  statistics(std::cout, g_count, split);
+  return 0;
+}
+
+/*
+void variance(std::ostream& os, const std::vector<int>& vals, int range) {
+  double avg = (range - 1) / 2.0; // the expected value is range / 2, which is average
+  double variance = 0.0;
+  for (size_t i = 0; i < vals.size(); ++i) {
+    variance += (vals[i] - avg) * (vals[i] - avg);
   }
 
-  delete [] g_count;
+  variance /= vals.size();
+  os << '[' << variance << ']';
+}
+*/
 
-  // probabilty: global, each player
-  //
+double statistics(std::ostream& os, const std::vector<int>& cnt, int split) {
+  int total = std::accumulate(cnt.begin(), cnt.end(), 0);
+  int part = cnt.size() / split;
+  double expected = 1.0 / split;
+  double variance = 0.0;
 
-  return 0;
+  std::vector<int> split_cnt(part, 0);
+  for (size_t i = 0; i < cnt.size(); ++i) {
+    int index = i / part;
+    split_cnt[index] += cnt[i];
+  }
+
+  os << '[' << total << ',' << std::setprecision(3) << expected << "]: ";
+  // [total, 1/split]: c1(p1), c2(p2), ... 
+  for (int i = 0; i < split; ++i) {
+    double prob = (double)split_cnt[i] / total;
+    os << split_cnt[i] << '(' << std::setprecision(3) << prob << ") ";
+    double diff = prob - expected;
+    variance += diff * diff;
+  }
+
+  variance /= split;
+  variance = sqrt(variance);
+  os << '[' << variance << "]\n";
+  return variance;
 }
 
 /*
